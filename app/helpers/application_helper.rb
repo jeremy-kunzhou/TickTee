aaa519a9f7dcd4ee899415c5ff9fb3ce6a14ce54
kun zhou <kun.paul.zhou@gmail.com>
2014-07-05 16:08:44 +1000

    change title functionality for show project

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def title(*parts)
+    unless parts.empty?
+      content_for :title do
+        (parts << "Ticketeef").join(" - ")
+      end
+      p @titlet
+    end
+  end
 end
---
+++ spec/features/generate_progress_image_spec.rb
@@ -0,0 +1,6 @@
+require 'rails_helper'
+
+feature 'Generate progress image' do
+  scenario 'can generate image of progress' do
+  end
+end
