578b298ca0fdaf4f96b0224f73692809533d5801
kun zhou <kun.paul.zhou@gmail.com>
2014-07-06 18:35:14 +1000

    fix bug in imgkit configuration file

--- config/initializers/imgkit.rb
+++ config/initializers/imgkit.rb
@@ -1,4 +1,4 @@
 # config/initializers/imgkit.rb
 IMGKit.configure do |config|
-	config.wkthmltoimage = Rails.root.join('bin', 'wkhtmltoimage').to_s if ENV['RACK_ENV'] == 'production'
+	config.wkhtmltoimage = Rails.root.join('bin', 'wkhtmltoimage').to_s if ENV['RACK_ENV'] == 'production'
 end
