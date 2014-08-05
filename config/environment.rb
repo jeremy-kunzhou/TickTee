482e6de43a641a11ce7498097a68253c47239130
kun zhou <kun.paul.zhou@gmail.com>
2014-08-06 04:05:29 +1000

    fix mailer

--- config/environment.rb
+++ config/environment.rb
@@ -4,3 +4,13 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 # ENV['RAILS_ENV'] ||= 'production'
+
+ActionMailer::Base.smtp_settings = {
+  address: 'smtp.sendgrid.net',
+  port: '587',
+  authentication: :plain,
+  user_name: 'app26655623@heroku.com',
+  password: 'wfcgboik',
+  domain: 'heroku.com',
+  enable_starttls_auto: true
+}
