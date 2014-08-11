8dce57bb523734938b510e7727672c6c34a7a91e
kun zhou <kun.paul.zhou@gmail.com>
2014-08-11 22:29:58 +0800

    create project via api with token

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -2,4 +2,20 @@ class Api::V1::ProjectsController < Api::V1::BaseController
   def index
     respond_with(current_user.projects)
   end
+  
+  def create
+    @project = current_user.projects.build(project_params)
+    respond_to do |format|
+      if @project.save
+        format.json { render json: @project.to_json,  success: true, status: :created}
+      else
+        format.json { render json: @project.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+  
+  private 
+  def project_params
+    params.require(:project).permit(:name, :description, :start_at, :end_at, :expected_progress, :current_progress)
+  end
 end
--- spec/api/v1/projects_spec.rb
+++ spec/api/v1/projects_spec.rb
@@ -5,15 +5,15 @@ describe "/api/v1/projects", type: :api do
   let!(:token) {user.authentication_token}
   let!(:project) {FactoryGirl.create(:project, user: user)}
   let!(:project_other) {FactoryGirl.create(:project, name: "Others Project")}
-  
+  let!(:headers) {{'HTTP_X_API_EMAIL' => user.email, 'HTTP_X_API_TOKEN' => token}}
   before :each do
     user.confirm!
   end
   
-  context "projects viewable by this user" do
+  context "projects view" do
     let(:url) { "/api/v1/projects"}
-    it "json" do
-      get "#{url}.json", {}, {'HTTP_X_API_EMAIL' => user.email, 'HTTP_X_API_TOKEN' => token}
+    it "projects viewable by this user" do
+      get "#{url}.json", {}, headers
       
       projects_json = user.projects.to_json
       expect(last_response.body).to eq(projects_json)
@@ -24,10 +24,41 @@ describe "/api/v1/projects", type: :api do
       projects.any? do |p|
         p["name"] == project.name
       end.should eql(true)
+    end
+    
+    it "projects belong to other user unviewable by this user" do
+      get "#{url}.json", {}, headers
+      
+      projects_json = user.projects.to_json
+      expect(last_response.body).to eq(projects_json)
+      expect(last_response.status).to eq(200)
+      
+      projects = JSON.parse(last_response.body)   
       
       projects.any? do |p|
         p["name"] == "Others Project"
       end.should eql(false)
+    end
+  end
+  
+  context "projects create" do 
+    let(:url) { "/api/v1/projects"} 
+    let(:new_project) { 
+      {
+        name: "Example project", 
+        description: "Description for project", 
+        start_at: "2014-07-01", 
+        end_at: "2014-08-01", 
+        expected_progress: 20,
+        current_progress: 10,
+        user_id: user.id
+      }
+    }
+    it "create projects" do 
+      post "#{url}.json", {project: new_project}, headers
+      
+      expect(last_response.status).to eq(201)
+      expect(last_response.body).to include(new_project[:name])
     end
   end
 end
